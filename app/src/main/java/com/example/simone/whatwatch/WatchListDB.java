485b178eeaab836f673295a9fa48d1308cc2965d
Kirar <raimondi-marco@hotmail.com>
2017-07-31 18:13:36 +0200

    Classe per il db

--- app/src/main/java/com/example/simone/whatwatch/WatchListDB.java
+++ app/src/main/java/com/example/simone/whatwatch/WatchListDB.java
@@ -11,12 +11,20 @@ public class WatchListDB {
     public static final int DB_VERSION = 1;
 
     //Costanti della tabella FilmDescription
-    public static final String FILM_TABLE = "film";
+    public static final String FILM_TABLE = "film_data";
 
     public static final String FILM_ID = "film_id";
     public static final int FILM_ID_COL = 0;
 
     public static String FILM_NAME = "film_name";
     public static final int FILM_NAME_COL = 1;
+
+    public static final String CREATE_FILM_TABLE =
+            "CREATE TABLE" + FILM_TABLE + " (" + FILM_ID + " TEXT   NOT NULL UNIQUE);";
+
+    public static final String DROP_FILM_TABLE =
+            "DROP TABLE IF EXISTS" + FILM_TABLE;
+
+
 
 }
