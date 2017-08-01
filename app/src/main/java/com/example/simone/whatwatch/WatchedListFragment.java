93bc7378838e255c26afe9915e353185907a4021
Marco <theovation95@gmail.com>
2017-08-01 11:22:26 +0200

    watchedfragment

--- app/src/main/java/com/example/simone/whatwatch/WatchedListFragment.java
+++ app/src/main/java/com/example/simone/whatwatch/WatchedListFragment.java
@@ -8,6 +8,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import database.FilmDescriptionDB;
 
 
 public class WatchedListFragment extends Fragment {
@@ -19,7 +22,10 @@ public class WatchedListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_watched_list, container, false);
 
-
+        FilmDescriptionDB[] filmDescriptionDB = new FilmDescriptionDB[10];
+        GridView gridView = (GridView) view.findViewById(R.id.gridview);
+        WatchlistAdapter watchlistAdapter = new WatchlistAdapter(getActivity(), filmDescriptionDB);
+        gridView.setAdapter(watchlistAdapter);
 
         return view;
     }
