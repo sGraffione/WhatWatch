4cc1d0732e4c4ff231b77c0e5a6eee959a1fcc96
uDragun <simo.graffio@gmail.com>
2017-08-04 12:16:17 +0200

    Risolto errore refresh watchlist

--- app/src/main/java/com/example/simone/whatwatch/downloadJSONInfo.java
+++ app/src/main/java/com/example/simone/whatwatch/downloadJSONInfo.java
@@ -40,7 +40,6 @@ public class downloadJSONInfo extends AsyncTask<String, Void, Void> {
         pDialog.setMessage("Retrieving information...");
         pDialog.setCancelable(false);
         pDialog.show();
-
     }
 
 
